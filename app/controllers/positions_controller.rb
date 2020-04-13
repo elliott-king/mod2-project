7a1ae3ce33b39013e492a61b780abfcac11265e5
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-04-13 17:07:36 -0400

    Index & show for positions

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,2 +1,16 @@
 class PositionsController < ApplicationController
+  def index
+    @positions = Position.all 
+  end
+  def show
+    @position = Position.find(params[:id])
+  end
+  def new
+  end
+  def create
+  end
+  def edit
+  end
+  def update
+  end
 end
