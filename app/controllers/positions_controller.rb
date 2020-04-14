a1133267aa21ffc246a1947adfae975117ffa5d7
Yueying <wangyy395@gmail.com>
2020-04-14 10:34:24 -0400

    add link to position's show page

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -21,15 +21,23 @@ class PositionsController < ApplicationController
       redirect_to new_position_path
     end
   end
-
+  
   def edit
     @position = Position.find(params[:id])
   end
+
   def update
+    if position = Position.update(position_params)
+      redirect_to position_path(position)
+    else
+      flash[:errors] = position.errors.full_messages
+      redirect_to edit_position_path
+    end    
   end
 
   private
   def position_params
     params.require(:position).permit!
   end
+  
 end
