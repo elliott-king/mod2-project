5692274a2759d8cc2f64ff7975e01c3502f400f9
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-04-14 10:24:33 -0400

    Basic application functionality No index as of yet

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,2 +1,36 @@
 class ApplicationsController < ApplicationController
+  def show
+    @application = Application.find(params[:id])
+  end
+  def new
+    @application = Application.new 
+  end
+  def create
+    application = Application.create(application_params)
+    if application.valid? 
+      redirect_to application_path(application)
+    else
+      flash[:errors] = application.errors.full_messages
+      redirect_to new_application_path
+    end
+  end
+  def edit
+    @application = Application.find(params[:id])
+  end
+  def update
+    application = Application.find(params[:id])
+    application.update(application_params)
+    if application.valid?
+      redirect_to application_path(application)
+    else
+      flash[:errors] = application.errors.full_messages
+      redirect_to edit_application_path(application)
+    end
+  end
+  # todo: delete
+
+  private
+  def application_params
+  params.require(:application).permit(Application.column_names)
+  end
 end
