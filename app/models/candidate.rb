4f825517d244961cc54ce509974ffa36debeb275
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-04-15 13:45:54 -0400

    Bugfix: 'positions' misspelled

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,6 +1,6 @@
 class Candidate < ActiveRecord::Base
     has_many :applications
-    has_many :postions, through: :applications
+    has_many :positions, through: :applications
 
     validates :name, :gpa, :education_level, :native_language, presence: true
     validates :gpa, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 5.5}
